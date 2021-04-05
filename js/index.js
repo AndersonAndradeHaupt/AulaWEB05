f07d8c8ca1c611af5e706786185336c469536021
FyodaHaupt <80731486+FyodaHaupt@users.noreply.github.com>
2021-04-05 16:42:46 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function subimit_form_login(){
+    if (document.form_login.input_email.value  == 'fyoda_anders@hotmail.com'){
+        if (document.form_login.input_senha.value = '1122'){
+        alert("Bem vindo a seu site!")
+    } else {
+        alert("Você errou sua senha!")
+    }
+    } else {
+        alert("Este domínio não existe!")
+    }
+
+}
